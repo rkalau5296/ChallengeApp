7e7e5be409c6400bcbe666392fa9a2d133ed8dce
rafal <rafal.kalata.itservice@gmail.com>
2023-09-14 22:56:31 +0200

    Dzien1

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string gender = "K";
+int age = 33;
+
+if (gender == "K" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (gender == "M" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else
+{
+    Console.WriteLine("" + name + ", lat " + age + "");
+}
+
