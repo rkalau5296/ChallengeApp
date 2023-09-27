63984d1560a2c276975c68dc985f6e75509cd937
rafal <rafal.kalata.itservice@gmail.com>
2023-09-27 22:21:39 +0200

    change to float

--- ChallengeApp/ChallengeApp/Supervisor.cs
+++ ChallengeApp/ChallengeApp/Supervisor.cs
@@ -9,7 +9,14 @@
 
         public void AddGrade(float grade)
         {
-            throw new NotImplementedException();
+            if (grade > 0 && grade <= 100)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                throw new Exception("Invalid grade value");
+            }
         }
 
         public void AddGrade(string grade)
@@ -19,62 +26,62 @@
                 switch (grade)
                 {
                     case "6":
-                        this.grades.Add(100);
+                        this.AddGrade(100);
                         break;
                     case "6-":
                     case "-6":
-                        this.grades.Add(95);
+                        this.AddGrade(95);
                         break;
                     case "5+":
                     case "+5":
-                        this.grades.Add(85);
+                        this.AddGrade(85);
                         break;
                     case "5":
-                        this.grades.Add(80);
+                        this.AddGrade(80);
                         break;
                     case "5-":
                     case "-5":
-                        this.grades.Add(75);
+                        this.AddGrade(75);
                         break;
                     case "4+":
                     case "+4":
-                        this.grades.Add(65);
+                        this.AddGrade(65);
                         break;
                     case "4":
-                        this.grades.Add(60);
+                        this.AddGrade(60);
                         break;
                     case "4-":
                     case "-4":
-                        this.grades.Add(55);
+                        this.AddGrade(55);
                         break;
                     case "3+":
                     case "+3":
-                        this.grades.Add(45);
+                        this.AddGrade(45);
                         break;
                     case "3":
-                        this.grades.Add(40);
+                        this.AddGrade(40);
                         break;
                     case "3-":
                     case "-3":
-                        this.grades.Add(35);
+                        this.AddGrade(35);
                         break;
                     case "2+":
                     case "+2":
-                        this.grades.Add(25);
+                        this.AddGrade(25);
                         break;
                     case "2":
-                        this.grades.Add(20);
+                        this.AddGrade(20);
                         break;
                     case "2-":
                     case "-2":
-                        this.grades.Add(15);
+                        this.AddGrade(15);
                         break;
                     case "1+":
                     case "+1":
-                        this.grades.Add(5);
+                        this.AddGrade(5);
                         break;
                     case "1":
-                        this.grades.Add(0);
+                        this.AddGrade(0);
                         break;
                     default:
                         throw new Exception("Wrong letter");
